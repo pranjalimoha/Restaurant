8572f635c1cd5d0abf90ee786395b1efc33c50b2
Pranjali <pranjali.m22@gmail.com>
2026-04-11 19:47:32 -0500

    initial commit for authentication

---
+++ BackEnd/src/controllers/authController.js
@@ -0,0 +1,72 @@
+import * as authService from '../services/authService.js';
+
+export const registerUser = async (req, res, next) => {
+    try {
+        const result = await authService.registerUser(req.body);
+        res.status(201).json({
+            success: true,
+            message: 'User registered successfully',
+            data: result,
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const userLogin = async (req, res, next) => {
+
+    try {
+        const { userEmail, userPassword } = req.body;
+        const result = await authService.userLogin(userEmail, userPassword);
+        res.status(200).json({
+            success: true,
+            message: 'User logged in successfully',
+            data: result,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const createGuestUser = async (req, res, next) => {
+
+    try {
+
+        const guestUser = await authService.createGuestUser(req.body);
+        res.status(201).json({
+            success: true,
+            message: 'Guest user created successfully',
+            data: guestUser,
+        });
+    }
+    catch (error) {
+        next(error);
+    }
+};
+
+export const upgradeGuest = async (req, res, next) => {
+    try {
+        const { userId } = req.params;
+        const result = await authService.upgradeGuestToRegistered(userId, req.body);
+
+        res.status(200).json({
+            success: true,
+            message: 'Guest upgraded to registered user successfully',
+            data: result,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getCurrentUser = async (req, res, next) => {
+    try {
+        res.status(200).json({
+            success: true,
+            data: req.user,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
---
+++ BackEnd/src/controllers/reservationController.js
@@ -0,0 +1,7 @@
+export const searchTableAvailability = async (req, res, next) => { }
+
+export const createReservation = async (req, res, next) => { }
+
+export const getReservationById = async (req, res, next) => { }
+
+export const cancelReservation = async (req, res, next) => { }
